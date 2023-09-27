6fe41b60cfffbe6d4aeccbb5e14ea68ba7cbc4b9
Rogério Alcantara <rogerio.alunos@cfp127aluno.local>
2023-09-27 20:19:06 -0300

    python-senai

---
+++ area_circ.py
@@ -0,0 +1,5 @@
+from math import pi
+
+raio = float(input("Digite a área: "))
+area_circ = pi * (raio**2)
+print(f"A circunferência de um círculo com raio {raio} é {area_circ:.2f}")
---
+++ convercao_dolar.py
@@ -0,0 +1,4 @@
+dolar = float(input('Informe o valor do dolar: '))
+taxa = float(input('Informe a taxa de câmbio: '))
+real = dolar * taxa
+print(f'A taxa de câmbio informada R${taxa} mais o valor em Dólar ${dolar} convertendo em Real fica: R${real} ')
